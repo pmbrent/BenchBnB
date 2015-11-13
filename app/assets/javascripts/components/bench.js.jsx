3ac99cf418cb4e6d766ab99e44f8ad27b801954f
pmbrent <patriciambrent@gmail.com>
2015-11-13 16:34:29 -0500

    Style changes.

--- app/assets/javascripts/components/bench.js.jsx
+++ app/assets/javascripts/components/bench.js.jsx
@@ -3,7 +3,7 @@ window.Bench = React.createClass({
   render: function() {
     return (
       <li>
-        <p>Latitutde: {this.props.bench.lat}, Longitude: {this.props.bench.lng}</p>
+        <p>Latitude: {this.props.bench.lat}, Longitude: {this.props.bench.lng}</p>
         <p>Description: {this.props.bench.description}</p>
       </li>
     );
