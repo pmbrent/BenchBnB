5e380fdcb3ed4a37237a2c50a2443f7d01595be7
pmbrent <patriciambrent@gmail.com>
2015-11-13 16:04:47 -0500

    Refactor to use bench id.

--- app/assets/javascripts/components/map.js.jsx
+++ app/assets/javascripts/components/map.js.jsx
@@ -23,42 +23,36 @@ window.Map = React.createClass({
   },
 
   makeMarkers: function() {
-    var benches = BenchStore.all();
     var LatLng;
-    var oldMarkers = this._markers.slice();
+    var benches = BenchStore.all();
+    var oldMarkers = [];
 
-    //check markers to see if a bench still matches them
-    //if so remove them from oldMarkers which will be deleted
-    //also remove the bench so it is not granted a duplicate marker
-    this._markers.forEach(function(marker) {
-      benches.some(function(bench) {
-
-      // Floating point comparisons are fun! Beware type coercion;
-      // toFixed returns a string!
-        if (marker.position.lat().toFixed(6) == bench.lat &&
-          marker.position.lng().toFixed(6) == bench.lng) {
-            benches.splice(benches.indexOf(bench), 1);
-            oldMarkers.splice(oldMarkers.indexOf(marker), 1);
-            return true;
-        }
+    oldMarkers = this._markers.filter(function(marker) {
+      return benches.every(function(bench) {
+        return marker.id !== bench.id;
       });
     });
-    
-    // Remove unneeded Markers using jQuery
-    this._markers = $(this._markers).not(oldMarkers).get();
 
-    // Make new Markers for remaining benches
+    this.removeMarkers(oldMarkers);
+
+    var ids = this._markers.map(function(marker) {
+      return marker.id;
+    });
+
     benches.forEach(function(bench) {
+      if (ids.indexOf(bench.id) < 0) {
       LatLng = {lat: bench.lat, lng: bench.lng};
       this._markers.push(new google.maps.Marker({map: this.googleMap, position: LatLng,
-        animation: google.maps.Animation.DROP}));
+        animation: google.maps.Animation.DROP, id: bench.id}));
+      }
      }.bind(this));
   },
 
   removeMarkers: function(oldMarkers) {
     oldMarkers.forEach(function(marker) {
       marker.setMap(null);
-    });
+      this._markers.splice(this._markers.indexOf(marker), 1);
+    }, this);
   },
 
   render: function() {
