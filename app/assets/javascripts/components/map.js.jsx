f1fd092236ca685486196d71b2970be7991d4217
pmbrent <patriciambrent@gmail.com>
2015-11-13 15:37:32 -0500

    Remove unwanted Markers the hard way.

--- app/assets/javascripts/components/map.js.jsx
+++ app/assets/javascripts/components/map.js.jsx
@@ -1,6 +1,7 @@
 window.Map = React.createClass({
 
   googleMap: undefined,
+  _markers: [],
 
   componentDidMount: function() {
     this.googleMap = new google.maps.Map(document.getElementById("map"), {
@@ -21,18 +22,43 @@ window.Map = React.createClass({
     });
   },
 
-  componentWillReceiveProps: function() {
-
-  },
-
   makeMarkers: function() {
     var benches = BenchStore.all();
     var LatLng;
+    var oldMarkers = this._markers.slice();
+
+    //check markers to see if a bench still matches them
+    //if so remove them from oldMarkers which will be deleted
+    //also remove the bench so it is not granted a duplicate marker
+    this._markers.forEach(function(marker) {
+      benches.some(function(bench) {
+
+      // Floating point comparisons are fun! Beware type coercion;
+      // toFixed returns a string!
+        if (marker.position.lat().toFixed(6) == bench.lat &&
+          marker.position.lng().toFixed(6) == bench.lng) {
+            benches.splice(benches.indexOf(bench), 1);
+            oldMarkers.splice(oldMarkers.indexOf(marker), 1);
+            return true;
+        }
+      });
+    });
+    
+    // Remove unneeded Markers using jQuery
+    this._markers = $(this._markers).not(oldMarkers).get();
+
+    // Make new Markers for remaining benches
     benches.forEach(function(bench) {
       LatLng = {lat: bench.lat, lng: bench.lng};
-      new google.maps.Marker({map: this.googleMap, position: LatLng,
-        animation: google.maps.Animation.DROP});
-    }.bind(this));
+      this._markers.push(new google.maps.Marker({map: this.googleMap, position: LatLng,
+        animation: google.maps.Animation.DROP}));
+     }.bind(this));
+  },
+
+  removeMarkers: function(oldMarkers) {
+    oldMarkers.forEach(function(marker) {
+      marker.setMap(null);
+    });
   },
 
   render: function() {
